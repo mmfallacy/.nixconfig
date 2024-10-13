4fd0badd5818e6159aa01e5a3d6c17a682ebe350
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-10-14 01:07:55 +0800

    refactor: move configuration specific code to host

--- flake.nix
+++ flake.nix
@@ -6,18 +6,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... } @ inputs:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-    	inherit system;
-      config.allowUnfree = true;
-    };
-    lib = nixpkgs.lib;
-  in {
-    nixosConfigurations.vm = lib.nixosSystem {
-      inherit system;
-      modules = [./hosts/vm/configuration.nix];
-    };
+  outputs = { self, nixpkgs, ... } @ inputs:{
+    nixosConfigurations.vm = import ./hosts/vm/configuration.nix {inherit inputs;};
   };
 }
--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -1,15 +1,24 @@
-{self, config, pkgs, ... }:
-
-{
-  imports = [
-    ../../modules/system/base.nix
-    ../../modules/system/grub.nix
-    ../../modules/system/pipewire.nix
-    ./hardware-configuration.nix
-    ./user.nix
-  ];
-
-  config.modules.grub.enable = true;
-  config.modules.pipewire.enable = true;
-
-}
+{inputs}:
+with inputs;
+let
+  system = "x86_64-linux";
+  pkgs = import nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+  };
+  lib = nixpkgs.lib;
+in
+  lib.nixosSystem {
+    inherit system;
+    modules = [
+      ../../modules/system/base.nix
+      ../../modules/system/grub.nix
+      ../../modules/audio/pipewire.nix
+      ./hardware-configuration.nix
+      ./user.nix
+      {
+        config.modules.grub.enable = true;
+        config.modules.pipewire.enable = true;
+      }
+    ];
+  }
