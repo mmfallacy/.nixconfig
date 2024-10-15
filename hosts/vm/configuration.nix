8103943dc4d9876c9c83f3a2138a2f085eebaf26
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-10-15 14:33:45 +0800

    feat: use system modules in config

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -1,25 +1,33 @@
-{config, R, pkgs, ...}: let
+{config, R, pkgs, ...} @ inputs: let
   USERNAME = "mmfallacy";
 in {
   imports = [
-    (R "/modules/system/base.nix")
-    (R "/modules/system/grub.nix")
-    (R "/modules/audio/pipewire.nix")
+    (R "/system")
   ];
 
   config = {
-    modules.grub.enable = true;
-    modules.pipewire.enable = true;
+    # Virtualization
+    virtualisation.vmware.guest.enable = true;
 
+    modules = {
+        pipewire.enable = true;
+        grub.enable = true;
+        gnome.enable = true;
+        gdm.enable = true;
+        gnupg.enable = true;
+    };
+
+    # User
     users.users.${USERNAME} = {
       isNormalUser = true;
       description = "Michael M.";
       extraGroups = [ "networkmanager" "wheel" ];
     };
-
     networking.hostName = USERNAME;
+
     home-manager.useGlobalPkgs = true;
     home-manager.useUserPackages = true;
+    home-manager.extraSpecialArgs = {inherit inputs;};
     home-manager.users.${USERNAME} = {
       home.packages = with pkgs; [
         kitty
