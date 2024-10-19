7260e3175465a5933878d209f93ecb0849b6d084
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-10-19 17:04:17 +0800

    docs: flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   description = "NixOS Configuration";
 
+  # Flake dependencies.
+  # `nixpkgs` is locked to NixOS Release 24.05.
+  # `nixpkgs-unstable` is locked to NixOS unstable.
+  # `home-manager` is locked to Release 24.05, with its nixpkgs following `nixpkgs`.
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -11,11 +15,19 @@
     };
   };
 
+  # Flake outputs.
+  # nixosConfigurations.{host} are valid targets for `nixos-rebuild`
   outputs = { self, ... } @ inputs:
   let
+    # This set contains utility functions defined in ./mylib.
     mylib = import ./mylib inputs;
+
+    # Function used to create hosts. mkHost also passes mylib to the host's configuration.nix.
     mkHost = mylib.mkHost {inherit inputs; inherit mylib;};
   in {
+
+    # To create a new host, insert a new line containing nixosConfigurations.{hostname} = mkHost "${hostname}" {system};
+    # e.g. nixosConfigurations.host = mkHost "host" "x86_64-linux";
     nixosConfigurations.vm = mkHost "vm" "x86_64-linux";
   };
 }
