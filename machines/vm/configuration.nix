5915cd5b2e3f7293b499985933008867d270634e
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-12-29 00:58:05 +0800

    chore: refactor home configuration entry point for vm machine

--- machines/vm/configuration.nix
+++ machines/vm/configuration.nix
@@ -1,5 +1,9 @@
 inputs @ {self, config, pkgs, ... }: let
   modules.system = import ../../system inputs;
+
+  profiles = import ../../profiles inputs;
+  inherit (profiles) mmfallacy;
+  inherit (mmfallacy.const) username name;
 in {
   imports = [
     modules.system.base
@@ -9,10 +13,20 @@ in {
     modules.system.login.gdm
     modules.system.wm.gnome
     ./hardware-configuration.nix
-    ./user.nix
   ];
 
   time.timeZone = "Asia/Manila";
   networking.hostName = "MMFALLACY-NixVM";
   virtualisation.vmware.guest.enable = true;
+
+  users.users.${username} = {
+    isNormalUser = true;
+    description = name;
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.${username} = import ./home.nix;
+  home-manager.extraSpecialArgs = with mmfallacy; { inherit const; baseConfig = homeConfig; };
 }
--- machines/vm/home.nix
+++ machines/vm/home.nix
@@ -1,8 +1,5 @@
-inputs @ { lib , ... }: let
-  profiles = import ../../profiles inputs;
-  inherit (profiles.mmfallacy.const) username name;
-  inherit (profiles.mmfallacy) homeConfig;
-in lib.recursiveUpdate (homeConfig inputs) {
-  programs.git.extraConfig.gpg.format = "ssh";
-  programs.git.signing.key = "~/.ssh/id_ed25519";
+inputs @ { lib , pkgs , baseConfig , ... }: {
+  imports = [
+    baseConfig
+  ];
 }
