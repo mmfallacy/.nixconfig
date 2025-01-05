0385ef1da64315daf43e3a2b0acdae06e8f7b5f8
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-01-06 00:56:38 +0800

    feat(user/shell): zsh plugins autopair, defer, fzf-tab

--- user/shell/zsh.nix
+++ user/shell/zsh.nix
@@ -1,10 +1,22 @@
 { pkgs, ... } : let
   plug = pkg: { name = pkg.pname; src = pkg.src; };
 in {
+  # Dependency of zsh-fzf-tab
+  programs.fzf.enable = true;
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+
     plugins = with pkgs; builtins.map plug [
+      zsh-autopair
+      zsh-defer
+      zsh-fzf-tab
     ];
+
+    initExtra = ''
+      zsh-defer source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+    '';
+
   };
 }
