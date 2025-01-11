95794dce1261cfe1bf557224ce80b00dd43ad0fa
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-01-11 16:11:44 +0800

    feat(zsh): specify file in plugin entry instead of sourcing manually. remove zsh-defer

--- user/shell/zsh.nix
+++ user/shell/zsh.nix
@@ -1,5 +1,17 @@
 { pkgs, ... } : let
-  plug = pkg: { name = pkg.pname; src = pkg.src; };
+  # Generate home-manager plugin entry given the package.
+  plug = pkg: rec {
+    name = pkg.pname;
+    src = pkg.src;
+    # If pkg has file use it otherwise use default name.
+    file = if pkg ? file then pkg.file else "${name}.plugin.zsh";
+  };
+
+  # By default home-manager resolve zsh-<name> plugins by sourcing share/zsh-<name>/zsh-<name>.plugin.zsh;
+  # This attrset allows me to specify the file, and add other attributes while keeping plug interop
+  my = {
+      zsh-fzf-tab = pkgs.zsh-fzf-tab // { file = "fzf-tab.plugin.zsh"; };
+  };
 in {
   # Dependency of zsh-fzf-tab
   programs.fzf.enable = true;
@@ -8,15 +20,11 @@ in {
     enable = true;
     enableCompletion = true;
 
+    # Map plugin entries to plug to create valid home-manager plugin entries
     plugins = with pkgs; builtins.map plug [
       zsh-autopair
-      zsh-defer
-      zsh-fzf-tab
+      my.zsh-fzf-tab
     ];
-
-    initExtra = ''
-      zsh-defer source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
-    '';
-
   };
+
 }
