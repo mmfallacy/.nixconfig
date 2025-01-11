b36949354bca6d01836ec877f387e17ec75c63a2
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-01-12 02:45:53 +0800

    feat(nvim): more neovim plugins

--- user/neovim/plugins.nix
+++ user/neovim/plugins.nix
@@ -1,4 +1,5 @@
-vimPlugins : with vimPlugins; [
+vimPlugins : with vimPlugins;
+[
   lazy-nvim
 
   # Common Dependencies
@@ -6,9 +7,17 @@ vimPlugins : with vimPlugins; [
 
   # Plugins
   oil-nvim
+  gitsigns-nvim
+  indent-blankline-nvim
 
-  # mini.nvim
-  { name = "mini.pairs"; path = mini-nvim; }
+  # blink-cmp and other snippet sources
+  blink-cmp
+  friendly-snippets
+
+  # Noice.nvim and dependencies
+  noice-nvim
+  nui-nvim
+  nvim-notify
 
   # telescope.nvim and extensions
   telescope-nvim
@@ -19,4 +28,23 @@ vimPlugins : with vimPlugins; [
   nvim-treesitter
   nvim-treesitter-context
   # NOTE: Parsers are handled by ./treesiter.nix.
-]
+] ++ (builtins.map (name: { inherit name; path = mini-nvim; }) [
+  # mini.nvim
+  # Nixpkgs ships mini-nvim completely, with no way to sparsely checkout individual subplugins
+  # Hence, we will create symlinks to the complete plugin path to mimic lazy.nvim's way of
+  # handling these plugins
+  "mini.ai"
+  # "mini.base16" will already be included via stylix.
+  "mini.cursorword"
+  "mini.files"
+  "mini.hipatterns"
+  "mini.indentscope"
+  "mini.splitjoin"
+  "mini.starter"
+  "mini.statusline"
+  "mini.surround"
+
+  # We will use this also as a blink.cmp snippet source
+  "mini.snippets"
+
+])
