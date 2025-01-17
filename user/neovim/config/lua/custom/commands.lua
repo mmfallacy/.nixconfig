30fc3ba9632741567de77b258163d7dc4c68f454
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-01-18 01:34:51 +0800

    feat(nvim): remove highlights on line numbers

--- user/neovim/config/lua/custom/commands.lua
+++ user/neovim/config/lua/custom/commands.lua
@@ -1,2 +1,12 @@
 -- Rename :EditQuery user command as it conflicts with :Explore when quickly using :E
 vim.api.nvim_del_user_command('EditQuery')
+
+-- Run on VeryLazy User Event
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'VeryLazy',
+  callback = function()
+    -- Override colors for Line Number Column
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { bg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { bg = 'NONE' })
+  end,
+})
