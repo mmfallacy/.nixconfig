c7627700472b31e4004aa68ec4fd4857e8b6f7fd
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-01-19 04:22:22 +0800

    feat(niri): enable portals

--- user/niri/default.nix
+++ user/niri/default.nix
@@ -14,4 +14,12 @@
   home.packages = with pkgs; [
     xwayland-satellite
   ];
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+    ];
+  };
 }
