e4f9f0163881353de6c3bf648245aa4905daa6ca
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-01-24 22:43:44 +0800

    nvim: ft-markdown: add live-preview.nvim

--- user/neovim/config/lua/custom/plugins/filetype/markdown.lua
+++ user/neovim/config/lua/custom/plugins/filetype/markdown.lua
@@ -1,5 +1,5 @@
 local utils = require('custom.utils')
-return utils.map_ft('markdown')({
+local M = utils.map_ft('markdown')({
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = {
@@ -9,3 +9,24 @@ return utils.map_ft('markdown')({
     opts = {},
   },
 })
+
+--- live-preview.nvim will be mostly used for markdown previews
+--- but to account for its HTML, AsciiDoc, SVG support, we also
+--- include it in the ft field
+
+M[#M + 1] = {
+  'brianhuster/live-preview.nvim',
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+  },
+  ft = { 'markdown', 'html', 'asciidoc', 'svg' },
+  config = function(_, opts)
+    require('livepreview.config').set(opts)
+  end,
+  opts = {
+    -- Do not open a browser by default
+    -- browser = nil,
+  },
+}
+
+return M
