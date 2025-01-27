0ed50ae942634b7fb7b93a24880e54a563670d3a
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-01-28 03:19:19 +0800

    nvim: lsp: extract lsp setup opts to table and create lsp_setup_handlers function that applies global and useropts with exec checking

--- user/neovim/config/lua/custom/plugins/lsp/setup.lua
+++ user/neovim/config/lua/custom/plugins/lsp/setup.lua
@@ -1,8 +1,38 @@
-return function(_, _)
+local function lsp_setup_handlers(handlers, global)
   local lsp = require('lspconfig')
 
-  lsp.lua_ls.setup({})
-  lsp.marksman.setup({})
-  lsp.nil_ls.setup({})
-  lsp.zls.setup({})
+  local not_installed = {}
+
+  for server, _opts in pairs(handlers) do
+    local opts = vim.tbl_deep_extend('force', global, _opts)
+    lsp[server].setup(opts)
+    if vim.fn.executable(lsp[server].cmd[1]) ~= 1 then
+      vim.list_extend(not_installed, { server })
+    end
+  end
+
+  if #not_installed > 0 then
+    vim.notify_once(
+      'Some configured language servers are not accessible!\nCheck if lsp executables are installed.\nMissing LSPS:'
+        .. vim.inspect(not_installed),
+      vim.log.levels.WARN
+    )
+  end
+end
+
+return function(_, _)
+  local on_attach = require('custom.plugins.lsp.keybinds')
+
+  global = {
+    on_attach = on_attach,
+  }
+
+  local handlers = {
+    ['lua_ls'] = {},
+    ['marksman'] = {},
+    ['nil_ls'] = {},
+    ['zls'] = {},
+  }
+
+  lsp_setup_handlers(handlers, global)
 end
