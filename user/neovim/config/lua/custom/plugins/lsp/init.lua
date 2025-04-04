487b85fc85f377cf18dfdc63985ad5a7a99e928e
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-04-05 05:01:54 +0800

    chore: remove unused user folder

--- user/neovim/config/lua/custom/plugins/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-local M = {
-  'neovim/nvim-lspconfig',
-}
-
-M.event = { 'BufReadPre', 'BufNewFile' }
-
-M.config = require('custom.plugins.lsp.setup')
-
-return M
--- user/shell/direnv.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-  };
-
-  # Enable shell integration inside the respective shell's .nix file.
-}
