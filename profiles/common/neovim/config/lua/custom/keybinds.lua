974b1cf466a0579c811cd783e6b11ffd5d94de75
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-05-15 23:53:56 +0800

    nvim: buffer delete and previous keybinds

--- profiles/common/neovim/config/lua/custom/keybinds.lua
+++ profiles/common/neovim/config/lua/custom/keybinds.lua
@@ -7,3 +7,6 @@ local map = vim.keymap.set
 -- Unbind Ctrl+C to unlearn Ctrl+C=Esc keymap from before
 -- I already have hardware-level remapped my Capslock to Esc
 map({ 'n', 'v', 'c', 'i' }, '<C-c>', '<nop>')
+
+map({ 'n', 'v', 'c', 'i' }, '<leader>bd', ':bp | bd # <CR>')
+map({ 'n', 'v', 'c', 'i' }, '<leader>bp', ':bp <CR>')
