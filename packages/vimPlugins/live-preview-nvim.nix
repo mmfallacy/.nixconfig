fbed621548e36564344e2a2aebdf024e7405350f
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-06-08 04:37:28 +0800

    fix: live-preview-nvim fails during nvimRequireCheckPhase
    This is because live-preview-nvim contains a `_spec.lua` which  contains test that checks if live-preview.nvim is  require-able in a neovim context. Since this hook runs within realisation, it fails since there is no neovim context.

--- packages/vimPlugins/live-preview-nvim.nix
+++ packages/vimPlugins/live-preview-nvim.nix
@@ -13,6 +13,8 @@ vimUtils.buildVimPlugin {
     rev = "330a6ac95a4eece731117ca5956250821bae8010";
     sha256 = "sha256-4RxXh3Qx03pW63SpDwhA0ALVkCWCRiR6OnAzurpp88o=";
   };
+  # [brianhuster/live-preview.nvim#L24](https://github.com/brianhuster/live-preview.nvim/blob/6de1e71572673918e29cfc012c8dbfa59a2e464b/lua/livepreview/_spec.lua#L24) fails as it trips during neovimRequireCheckHook. Packages are not installed within the neovim context during build hence this fails.
+  nvimSkipModules = [ "livepreview._spec" ];
   meta.homepage = "https://github.com/brianhuster/live-preview.nvim/";
   meta.license = lib.licenses.gpl3Only;
 
