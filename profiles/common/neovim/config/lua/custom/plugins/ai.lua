e205db6e8dd6c57a0efd991b22858e2b5200b5e4
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-06-13 00:59:40 +0800

    nvim: avante: disable avante on missing key

--- profiles/common/neovim/config/lua/custom/plugins/ai.lua
+++ profiles/common/neovim/config/lua/custom/plugins/ai.lua
@@ -15,6 +15,32 @@ local M = {
   },
 }
 
+function has_missing_key(provider)
+  if provider == 'claude' and vim.env.ANTHROPIC_API_KEY ~= nil then
+    return false
+  end
+  if provider == 'openai' and vim.env.OPENAI_API_KEY ~= nil then
+    return false
+  end
+  if provider == 'gemini' and vim.env.GOOGLE_API_KEY ~= nil then
+    return false
+  end
+
+  return true
+end
+
+function M.config(_, opts)
+  -- ENV KEY checking prior to staring avante
+  if has_missing_key(opts.provider) then
+    return vim.notify(
+      [[avante.nvim cannot find your API key for "]] .. opts.provider .. [[" provider!]],
+      vim.log.levels.ERROR
+    )
+  else
+    require('avante').setup(opts)
+  end
+end
+
 M.opts = {
   provider = 'claude',
 }
