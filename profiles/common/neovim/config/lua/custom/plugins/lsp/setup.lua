7d94788e765c7154d686d11c8122de3bad8af003
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-06-30 00:29:55 +0800

    nvim: lsp: document lsp_setup_handlers function

--- profiles/common/neovim/config/lua/custom/plugins/lsp/setup.lua
+++ profiles/common/neovim/config/lua/custom/plugins/lsp/setup.lua
@@ -1,15 +1,20 @@
+-- Sets up lsp servers based on handlers table
 local function lsp_setup_handlers(handlers, global)
   local lsp = require('lspconfig')
   local not_installed = {}
 
   for server, _opts in pairs(handlers) do
+    -- Extend handler-specific opts with global
     local opts = vim.tbl_deep_extend('force', global, _opts)
     lsp[server].setup(opts)
+    -- Track servers that are not installed!
+    -- TODO: Instead output servers installed as most LSPs are installed through nix-shells
     if vim.fn.executable(lsp[server].cmd[1]) ~= 1 then
       vim.list_extend(not_installed, { server })
     end
   end
 
+  -- Warn when there are LSP servers that are not yet installed
   if #not_installed > 0 then
     vim.notify_once(
       'Some configured language servers are not accessible!\nCheck if lsp executables are installed.\nMissing LSPS:'
