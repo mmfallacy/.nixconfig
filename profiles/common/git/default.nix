e1e2fc6d48d253aca824ba61c1bf934fb67a6f20
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-07-01 02:37:10 +0800

    common: git: enable rerere

--- profiles/common/git/default.nix
+++ profiles/common/git/default.nix
@@ -22,6 +22,10 @@
       # Currently, I primarily use nvim hence I'm setting nvim as the default diff and merge tool
       diff.tool = lib.mkDefault "nvimdiff";
       merge.tool = lib.mkDefault "nvim";
+
+      # Automatically resolve conflict using previously recorded resolution
+      rerere.enabled = true;
+      rerere.autoUpdate = true;
     };
 
     # TODO: Think of a way to make this not user-specific.
