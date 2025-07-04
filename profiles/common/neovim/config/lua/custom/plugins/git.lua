59473fd03caa7d391d27ed433d8fef2be7a85dd8
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-07-05 02:57:39 +0800

    nvim: neogit:  neogit options on commit_editor

--- profiles/common/neovim/config/lua/custom/plugins/git.lua
+++ profiles/common/neovim/config/lua/custom/plugins/git.lua
@@ -14,7 +14,16 @@ local N = {
   opts = function()
     -- Create :Git alias for :Neogit
     vim.api.nvim_create_user_command('G', 'Neogit', {})
-    return {}
+    return {
+      commit_editor = {
+        kind = 'split_below',
+        staged_diff_split_kind = 'vsplit',
+      },
+      -- NOTE: Neogit currently doesn't support SSH signing.
+      -- All commits are signed via the git config option `commit.gpgSign = true`
+      -- Do note that signing can still work without gpg being present if `gpg.format = "ssh"`
+      -- commit_view = {verify_commit = nil,},
+    }
   end,
 }
 
