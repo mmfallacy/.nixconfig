e2fa331c9ff46406896ea13906ff17e990d4f83b
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-08-07 05:55:05 +0800

    nvim: fix nixnvim wrapper extensions for passing gemini api key

--- profiles/mmfallacy/neovim.nix
+++ profiles/mmfallacy/neovim.nix
@@ -8,11 +8,13 @@
 {
   home.packages =
     let
-      nixnvim = extras.nixnvim.overrideAttrs {
+      bin = pkg: lib.getExe pkgs.${pkg};
+
+      nixnvim = extras.nixnvim.override {
         wrapperArgs = [
           # Do note that --set VAR VAL treats VAL as an explicit string, hence no runtime evaluation.
           "--run"
-          ''export GEMINI_API_KEY="$(age --decrypt -i ~/.ssh/age.key ${extras.secrets.mmfallacy.GEMINI_API_KEY} | tr -d '\n')"''
+          ''export GEMINI_API_KEY="$(${bin "age"} --decrypt -i ~/.ssh/age.key ${extras.secrets.mmfallacy.GEMINI_API_KEY} | tr -d '\n')"''
         ];
 
         # Set vim alias for mmfallacy profile.
