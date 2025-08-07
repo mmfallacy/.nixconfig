bcdbbd3b9b1e0f83c220dbfd7c1ec81b177b038d
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-08-08 03:33:11 +0800

    nvim: add nvcd and refactor bin function

--- profiles/mmfallacy/neovim.nix
+++ profiles/mmfallacy/neovim.nix
@@ -8,18 +8,27 @@
 {
   home.packages =
     let
-      bin = pkg: lib.getExe pkgs.${pkg};
+      inherit (pkgs) age;
+
+      bin = pkg: lib.getExe pkg;
 
       nixnvim = extras.nixnvim.override {
         wrapperArgs = [
           # Do note that --set VAR VAL treats VAL as an explicit string, hence no runtime evaluation.
           "--run"
-          ''export GEMINI_API_KEY="$(${bin "age"} --decrypt -i ~/.ssh/age.key ${extras.secrets.mmfallacy.GEMINI_API_KEY} | tr -d '\n')"''
+          ''export GEMINI_API_KEY="$(${bin age} --decrypt -i ~/.ssh/age.key ${extras.secrets.mmfallacy.GEMINI_API_KEY} | tr -d '\n')"''
         ];
 
         # Set vim alias for mmfallacy profile.
         aliases = [ "vim" ];
       };
+
+      nvcd = pkgs.writeShellScriptBin "nvcd" ''
+        pushd "$1" && ${bin nixnvim} . "''${@:2}" && popd
+      '';
     in
-    [ nixnvim ];
+    [
+      nixnvim
+      nvcd
+    ];
 }
