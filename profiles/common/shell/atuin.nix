2c514d0ca95023475407160cd531400b033acb90
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-08-28 20:30:08 +0800

    atuin: reenable <A-/>

--- profiles/common/shell/atuin.nix
+++ profiles/common/shell/atuin.nix
@@ -1,19 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.atuin = {
     enable = true;
-    enableZshIntegration = true;
     settings = {
       auto_sync = false;
-      search_mode = "prefix";
-      keymap_mode = "vim-normal";
+      keymap_mode = "vim-insert";
       # Use <C-1> to <C-9> to navigate instead of <A-1> to <A-9>
       ctrl_n_shortcuts = true;
+      workspaces = true;
     };
     flags = [
-      # Use vim-normal "/" or vim-insert "<A-/>" instead!
       "--disable-up-arrow"
       "--disable-ctrl-r"
     ];
   };
+
+  programs.zsh.initContent =
+    pkgs.lib.mkAfter # bash
+      ''
+        # Use vim-normal "/" or vim-insert "<A-/>" instead!
+        bindkey -M vicmd '/' atuin-search
+      '';
 }
