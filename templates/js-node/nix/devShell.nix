67883afffd32e4d89752a708d7bf3405317c64eb
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-08-28 16:58:24 +0800

    templates: js-node: update node and include all files in ./nix to direnv watch

--- templates/js-node/nix/devShell.nix
+++ templates/js-node/nix/devShell.nix
@@ -4,18 +4,17 @@
 pkgs.mkShell {
   name = "js-pnpm-template";
 
-  nativeBuildInputs =
-    [
-      pkgs.nodejs_23
-    ]
-    ++ pkgs.lib.mapAttrsToList (k: v: pkgs.writeShellScriptBin k v) {
-      npm = "corepack npm \$@";
-      npx = "corepack npx \$@";
+  nativeBuildInputs = [
+    pkgs.nodejs_24
+  ]
+  ++ pkgs.lib.mapAttrsToList (k: v: pkgs.writeShellScriptBin k v) {
+    npm = "corepack npm \$@";
+    npx = "corepack npx \$@";
 
-      yarn = "corepack yarn \$@";
-      yarnpkgs = "corepack yarnpkgs \$@";
+    yarn = "corepack yarn \$@";
+    yarnpkgs = "corepack yarnpkgs \$@";
 
-      pnpm = "corepack pnpm \$@";
-      pnpx = "corepack pnpx \$@";
-    };
+    pnpm = "corepack pnpm \$@";
+    pnpx = "corepack pnpx \$@";
+  };
 }
