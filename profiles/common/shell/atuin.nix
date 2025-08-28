b964e7e47f5f6d631f7af264a946cd1470223213
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-08-28 16:51:36 +0800

    atuin: disable ctrl r, and use ctrl 1-9 for navigation

--- profiles/common/shell/atuin.nix
+++ profiles/common/shell/atuin.nix
@@ -7,7 +7,13 @@
       auto_sync = false;
       search_mode = "prefix";
       keymap_mode = "vim-normal";
+      # Use <C-1> to <C-9> to navigate instead of <A-1> to <A-9>
+      ctrl_n_shortcuts = true;
     };
-    flags = [ "--disable-up-arrow" ];
+    flags = [
+      # Use vim-normal "/" or vim-insert "<A-/>" instead!
+      "--disable-up-arrow"
+      "--disable-ctrl-r"
+    ];
   };
 }
