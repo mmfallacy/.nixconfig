2e0361bdecf0370578fc54728690c9293631e563
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-09-17 02:57:50 +0800

    common: neovim: remove deprecated neovim configuration in favor of github:mmfallacy/.nixnvim

--- profiles/common/neovim/config/lua/custom/plugins/cmp/init.lua
+++
@@ -1,2 +0,0 @@
--- return require('custom.plugins.cmp.blink')
-return require('custom.plugins.cmp.nvim-cmp')
--- profiles/common/neovim/config/lua/custom/plugins/misc.lua
+++
@@ -1,3 +0,0 @@
-return {
-  { 'tpope/vim-sleuth', event = { 'BufReadPost', 'BufNewFile' } },
-}
