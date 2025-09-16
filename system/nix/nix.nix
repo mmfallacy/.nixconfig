bcfdc24f4af79d60cbcad771f8d11442a34eeb1c
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-09-17 00:14:17 +0800

    chore: disable lix substituter

--- system/nix/nix.nix
+++ system/nix/nix.nix
@@ -18,12 +18,12 @@
     substituters = [
       "https://nix-community.cachix.org/"
       "https://cache.nixos.org/"
-      "https://cache.lix.systems"
+      # "https://cache.lix.systems"
     ];
 
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+      # "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
     ];
 
   };
