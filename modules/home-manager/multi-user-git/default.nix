cd33764d339ef465e9b6cd4a8ecd67932f905d7f
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-10-01 02:15:06 +0800

    homeModules: multi-user-git: add authKey to user type, signingFormat to options; assert git is enabled

--- modules/home-manager/multi-user-git/default.nix
+++ modules/home-manager/multi-user-git/default.nix
@@ -1,18 +1,28 @@
-{ config, lib, ... }@args:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}@args:
 let
   cfg = config.custom.multi-user-git;
 in
 {
-  config =
-    lib.mkIf cfg.enable {
+  config = lib.mkIf cfg.enable (
+    {
       assertions = [
         {
           assertion = builtins.length cfg.users > 0;
-          message = "multi-user-git enabled but no additional users supplied";
+          message = "multi-user-git enabled but no users supplied";
+        }
+        {
+          assertion = config.programs.git.enable;
+          message = "programs.git is not enabled";
         }
       ];
     }
-    // import ./config.nix args;
+    // import ./config.nix args
+  );
 
   options.custom.multi-user-git =
     let
@@ -31,6 +41,10 @@ in
             type = lib.types.str;
             description = "Git signing key";
           };
+          authKey = lib.mkOption {
+            type = lib.types.str;
+            description = "Git auth key";
+          };
         };
       };
     in
@@ -40,9 +54,20 @@ in
       users = lib.mkOption {
         default = [ ];
         description = ''
-          List of users to add on top of the default const.username and const.email
+          List of users to add. Note that this module removes const.username and const.email from the global git config
         '';
         type = types.listOf userType;
+      };
+
+      signingFormat = lib.mkOption {
+        default = "ssh";
+        description = ''
+          Set global signing format of users.*.signingKey
+        '';
+        type = types.enum [
+          "ssh"
+          "gpg"
+        ];
       };
     };
 }
