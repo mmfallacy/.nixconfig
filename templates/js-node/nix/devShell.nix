5c00f07989d0c348e29fc463629372ba88077d1d
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-10-07 16:35:07 +0800

    templates: js-node: add lsps as part of template

--- templates/js-node/nix/devShell.nix
+++ templates/js-node/nix/devShell.nix
@@ -4,17 +4,24 @@
 pkgs.mkShell {
   name = "js-pnpm-template";
 
-  nativeBuildInputs = [
-    pkgs.nodejs_24
-  ]
-  ++ pkgs.lib.mapAttrsToList (k: v: pkgs.writeShellScriptBin k v) {
-    npm = "corepack npm \$@";
-    npx = "corepack npx \$@";
+  nativeBuildInputs =
+    with pkgs;
+    [
+      nodejs_24
+      vtsls
+      # vscode-langservers-extracted
+      # nodePackages.prettier
+      # emmet-language-server
+      # tailwindcss-language-server
+    ]
+    ++ pkgs.lib.mapAttrsToList (k: v: pkgs.writeShellScriptBin k v) {
+      npm = "corepack npm \$@";
+      npx = "corepack npx \$@";
 
-    yarn = "corepack yarn \$@";
-    yarnpkgs = "corepack yarnpkgs \$@";
+      yarn = "corepack yarn \$@";
+      yarnpkgs = "corepack yarnpkgs \$@";
 
-    pnpm = "corepack pnpm \$@";
-    pnpx = "corepack pnpx \$@";
-  };
+      pnpm = "corepack pnpm \$@";
+      pnpx = "corepack pnpx \$@";
+    };
 }
