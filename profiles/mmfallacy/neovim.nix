e8a5c63f5c7160a0cb40846ea0d91282f96e496d
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-10-11 04:44:45 +0800

    nixnvim: use raw gemini-cli and inject api key from neovim-injected key

--- profiles/mmfallacy/neovim.nix
+++ profiles/mmfallacy/neovim.nix
@@ -12,16 +12,20 @@
 
       bin = pkg: lib.getExe pkg;
 
-      nixnvim = extras.nixnvim.neovim.override {
+      nixnvim = extras.nixnvim.neovim.override (prev: {
         wrapperArgs = [
           # Do note that --set VAR VAL treats VAL as an explicit string, hence no runtime evaluation.
           "--run"
           ''export GEMINI_API_KEY="$(${bin age} --decrypt -i ~/.ssh/age.key ${extras.secrets.mmfallacy.GEMINI_API_KEY} | tr -d '\n')"''
         ];
 
+        extraBinPath = prev.extraBinPath ++ [
+          extras.nixnvim.gemini-cli
+        ];
+
         # Set vim alias for mmfallacy profile.
         aliases = [ "vim" ];
-      };
+      });
 
       # nixnvim based on ~/.nixnvim/nvimrc/
       nvim = nixnvim.devMode;
@@ -63,10 +67,27 @@
             --run 'export GEMINI_API_KEY="$(${bin age} --decrypt -i ~/.ssh/age.key ${extras.secrets.mmfallacy.GEMINI_API_KEY} | tr -d '\n')"'
         '';
       };
+
+      gemini-cli = pkgs.stdenvNoCC.mkDerivation {
+        name = "gemini-cli-wrapped";
+        dontUnpack = true;
+        buildInputs = [
+          pkgs.makeWrapper
+        ];
+        runtimeInputs = [
+          pkgs.age
+        ];
+        installPhase = ''
+          mkdir -p $out/bin
+          makeWrapper ${bin extras.nixnvim.gemini-cli} $out/bin/gemini-cli \
+            --run 'export GEMINI_API_KEY="$(${bin age} --decrypt -i ~/.ssh/age.key ${extras.secrets.mmfallacy.GEMINI_API_KEY} | tr -d '\n')"'
+        '';
+      };
+
     in
     [
       nvim
-      extras.nixnvim.gemini-cli
+      gemini-cli
       aider
       ngit
       nvcd
