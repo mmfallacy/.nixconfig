1a35b98660cfeb487d9c8fe66a5b4fcfc4756380
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-11-29 20:43:34 +0800

    feat: use self.homeModules.zellij to bootstrap unlock-first keybinds and tooltips

---
+++ profiles/common/shell/zellij/default.nix
@@ -0,0 +1,32 @@
+{ pkgs, extras, ... }:
+{
+  # Use self.homeModules.zellij.nix insetad of inputs.home-manager's zellij.nix
+  disabledModules = [ "${extras.hm}/modules/programs/zellij.nix" ];
+
+  imports = [
+    ./keybinds.nix
+  ];
+
+  programs.zellij = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      simplified_ui = true;
+      default_layout = "compact";
+      default_mode = "locked";
+      show_startup_tips = false;
+      pane_frames = false;
+      on_force_close = "quit";
+    };
+    exitShellOnExit = true;
+
+    extraConfig = ''
+      plugins {
+        compact-bar location="zellij:compact-bar" {
+          tooltip "F1"
+        }
+      }
+    '';
+
+  };
+}
