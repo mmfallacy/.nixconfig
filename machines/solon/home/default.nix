ea17669999c8b625ddf8173f204bcdb80af28635
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-01-24 03:00:31 +0800

    solon: NH_FLAKE

--- machines/solon/home/default.nix
+++ machines/solon/home/default.nix
@@ -19,6 +19,7 @@
   home.sessionVariables = rec {
     DOTFILES_PATH = with const; "/home/${username}/${dotfiles}";
     FLAKE = DOTFILES_PATH;
+    NH_FLAKE = DOTFILES_PATH;
   };
 
   home.packages = [
