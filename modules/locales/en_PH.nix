f8c0dbe62c93fa7ef9d6784d283a8e4189a51e36
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-03-01 15:49:32 +0800

    locales: migrate to flake-parts

---
+++ modules/locales/en_PH.nix
@@ -0,0 +1,18 @@
+{
+  flake.nixosModules.locale_en_PH =
+    { config, ... }:
+    {
+      i18n.defaultLocale = "en_PH.UTF-8";
+      i18n.extraLocaleSettings = {
+        LC_ADDRESS = "fil_PH";
+        LC_IDENTIFICATION = "fil_PH";
+        LC_MEASUREMENT = "fil_PH";
+        LC_MONETARY = "fil_PH";
+        LC_NAME = "fil_PH";
+        LC_NUMERIC = "fil_PH";
+        LC_PAPER = "fil_PH";
+        LC_TELEPHONE = "fil_PH";
+        LC_TIME = "fil_PH";
+      };
+    };
+}
