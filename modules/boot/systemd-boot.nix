bb6f6b9ba5921f2117c17e05d9958b5102e497fe
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-03-01 15:49:25 +0800

    boot: migrate to flake-parts

---
+++ modules/boot/systemd-boot.nix
@@ -0,0 +1,16 @@
+{
+  flake.nixosModules.systemd-boot =
+    { config, ... }:
+    {
+      boot.loader.grub.enable = true;
+      boot.loader.grub.device = "/dev/sda";
+      boot.loader.grub.useOSProber = true;
+
+      assertions = [
+        {
+          assertion = !config.boot.loader.systemd-boot.enable;
+          message = "grub conflicts with systemd-boot";
+        }
+      ];
+    };
+}
