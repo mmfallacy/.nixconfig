0778c29a955d7b0cdd84ced665ec938c188328dc
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-03-05 15:01:14 +0000

    fix: ly: initial_info_text pulls from wrong option

--- modules/login/ly.nix
+++ modules/login/ly.nix
@@ -6,14 +6,14 @@
       ...
     }:
     let
-      capitalize = str: with pkgs.lib; toUpper (substring 0 1 str) + substring 1 (-1) s;
+      capitalize = str: with pkgs.lib; toUpper (substring 0 1 str) + substring 1 (-1) str;
     in
     {
       services.displayManager.ly = {
         enable = true;
         settings = {
           animation = "matrix";
-          initial_info_text = capitalize config.networking.hostname;
+          initial_info_text = capitalize config.networking.hostName;
           text_in_center = "true";
           min_refresh_delta = 2;
 
