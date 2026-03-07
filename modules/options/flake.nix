c58bf37191f270ab0ec514b933f7af2c38ea6e8a
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-03-07 03:14:57 +0000

    flake: options for hjem modules and profiles; lib

---
+++ modules/options/flake.nix
@@ -0,0 +1,32 @@
+{ lib, ... }:
+let
+  inherit (lib) mkOption;
+  inherit (lib.types)
+    attrsOf
+    deferredModule
+    lazyAttrsOf
+    raw
+    ;
+in
+{
+  options.flake = {
+    hjemModules = mkOption {
+      type = attrsOf deferredModule;
+      default = { };
+      description = "Hjem modules for user-level packages";
+    };
+
+    hjemProfiles = mkOption {
+      type = attrsOf deferredModule;
+      default = { };
+      description = "Hjem profiles that compose hjem modules to build user profiles";
+    };
+
+    lib = mkOption {
+      type = lazyAttrsOf raw;
+      default = { };
+      description = "Library functions which require pkgs";
+    };
+
+  };
+}
