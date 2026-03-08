7d8576ddcfd47b5ea938c1bd2429c4f8d5665e4a
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-03-08 15:06:52 +0000

    zsh: nixosModule for global config

---
+++ modules/shell/zsh.nix
@@ -0,0 +1,27 @@
+{
+  flake.nixosModules.zsh =
+    { ... }:
+    {
+      programs.zsh = {
+        enable = true;
+        autosuggestions.enable = true;
+        syntaxHighlighting.enable = true;
+        enableCompletion = true;
+
+        setOptions = [
+          "HIST_IGNORE_DUPS"
+          "SHARE_HISTORY"
+          "EXTENDED_HISTORY"
+          # `cd`s push to directory stack
+          "AUTO_PUSHD"
+          "PUSHD_IGNORE_DUPS"
+          # show list on ambiguous completion.
+          "AUTO_LIST"
+          # move cursor to end of completed word.
+          "ALWAYS_TO_END"
+          # show completion menu on successive tab press
+          "AUTO_MENU"
+        ];
+      };
+    };
+}
