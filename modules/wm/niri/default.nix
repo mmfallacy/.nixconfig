a0e6ba654d0be83bba098b514e7172b012abab8a
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-03-14 14:36:47 +0000

    niri: undo weird hjem nesting separation

--- modules/wm/niri/default.nix
+++ modules/wm/niri/default.nix
@@ -1,4 +1,16 @@
 {
+  flake.nixosModules.niri =
+    { extras, ... }:
+    {
+      nixpkgs.overlays = [ extras.niri.overlays.niri ];
+      imports = [
+        extras.niri.nixosModules.niri
+      ];
+      programs.niri.enable = true;
+      # Use sodiboo's binary cache for niri. This is by default
+      niri-flake.cache.enable = true;
+    };
+
   flake.hjemModules.niri =
     { pkgs, ... }:
     {
