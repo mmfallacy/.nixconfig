be5b7c984809dc4ca530fd000fa461a006dd9cb0
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-03-15 04:10:11 +0000

    ly: setup_cmd now gets set within the ly service

--- modules/login/ly.nix
+++ modules/login/ly.nix
@@ -16,10 +16,6 @@
           initial_info_text = capitalize config.networking.hostName;
           text_in_center = "true";
           min_refresh_delta = 2;
-
-          # In >v1.0.2, wayland_cmd and x_cmd_setup is merged.
-          setup_cmd = "${config.services.displayManager.sessionData.wrapper}";
-
         };
       };
     };
