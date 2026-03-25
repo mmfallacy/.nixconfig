19230f596a1915c60f2a44df7103ee4c56b469bd
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-03-25 15:38:17 +0000

    nix: remove commented out lix sources and add public key for cache.nixos.org

--- modules/nix/nix.nix
+++ modules/nix/nix.nix
@@ -12,21 +12,20 @@
           "nix-command"
           "flakes"
           "pipe-operators"
-          # "pipe-operator"
         ];
 
         # Use nix-community cachix by default
-        # TODO: Investigate on whether this is a good idea or not.
         substituters = [
           "https://nix-community.cachix.org/"
           "https://cache.nixos.org/"
-          # "https://cache.lix.systems"
         ];
 
         trusted-public-keys = [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          # "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         ];
+
+        trusted-users = [ "root" ];
       };
 
       # Remove NIX_PATH and legacy channels
