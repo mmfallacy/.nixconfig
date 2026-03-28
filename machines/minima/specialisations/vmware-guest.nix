29fdd45368717a0ed0e3e6b624c12dd3a1a48fd1
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-03-28 10:55:51 +0000

    minima: specialisation use enable option

--- machines/minima/specialisations/vmware-guest.nix
+++ machines/minima/specialisations/vmware-guest.nix
@@ -1,17 +1,12 @@
 top: {
   flake.nixosModules.machine-minima =
     {
-      lib,
-      pkgs,
-      extras,
       ...
     }:
     {
       specialisation."VMWare Guest".configuration = {
         virtualisation.vmware.guest.enable = true;
-        imports = with top.config.flake.nixosModules; [
-          vmware-shared
-        ];
+        custom.system.vmware-shared.enable = true;
       };
     };
 }
