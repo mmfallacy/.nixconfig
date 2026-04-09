8d81e2e15a002756f89fe115c64a8d11ef29345c
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-04-09 16:26:51 +0000

    zsh: refactor plugins to manually load instead of using a custom loader

---
+++ modules/shell/zsh/plugins/zsh-autopair.nix
@@ -0,0 +1,30 @@
+{
+  flake.hjemModules.zsh =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    let
+      PLUGINS_DIR = ".zsh/plugins";
+    in
+    {
+      config = lib.mkIf config.custom.home.zsh.enable {
+        # source files
+        files."${PLUGINS_DIR}/zsh-autopair".source = pkgs.zsh-autopair;
+
+        # load
+        files.".zsh/.zshplugins".text = ''
+          path+="$HOME/${PLUGINS_DIR}/zsh-autopair"
+          fpath+="$HOME/${PLUGINS_DIR}/zsh-autopair"
+
+          plugin_path="$HOME/${PLUGINS_DIR}/zsh-autopair/share/zsh/zsh-autopair/autopair.zsh"
+          [[ -f "$plugin_path" ]] && source "$plugin_path"
+          unset plugin_path
+        '';
+
+        # dependencies
+      };
+    };
+}
