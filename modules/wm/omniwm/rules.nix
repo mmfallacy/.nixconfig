9de20e5dd5cb9ff892418f0136c6ca53f590a0e2
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-04-14 00:12:33 +0800

    omniwm: set 1password to float and set ios simulator to tile

--- modules/wm/omniwm/rules.nix
+++ modules/wm/omniwm/rules.nix
@@ -15,6 +15,16 @@
               id = "0FF5E590-D317-49BB-943B-C2905F43D948";
               assignToWorkspace = "9";
             }
+            {
+              bundleId = "com.1password.1password";
+              id = "6790050D-888B-4401-AA43-55386E4C0F8F";
+              layout = "float";
+            }
+            {
+              bundleId = "com.apple.iphonesimulator";
+              id = "443C1A29-119D-42EC-99BE-20B2E2025822";
+              layout = "tile";
+            }
           ];
         };
       };
