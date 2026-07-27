a70d2232aaadff258b5d2c4f702e17d0de6be80d
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-07-27 22:52:35 +0800

    niri: use binary cache for niri

--- modules/wm/niri/default.nix
+++ modules/wm/niri/default.nix
@@ -19,6 +19,16 @@
         # Use sodiboo's binary cache for niri. This is by default
         niri-flake.cache.enable = true;
       };
+
+      nix.settings = {
+        substituters = [
+          "https://niri-nix.cachix.org"
+        ];
+
+        trusted-public-keys = [
+          "niri-nix.cachix.org-1:SvFtqpDcf7Sm1SMJdby1/+Y+6f3Yt3/3PMcSTKPJNJ0="
+        ];
+      };
     };
 
   flake.hjemModules.niri =
