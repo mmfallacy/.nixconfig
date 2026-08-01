56adec896df5ff9368af2cd66693b416a4cddad9
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-08-01 15:08:56 +0000

    docs: better sample flakeparts module

--- modules/sample.nix
+++ modules/sample.nix
@@ -1,4 +1,27 @@
-{ inputs, ... }:
 {
+  flake.nixosModules.sample =
+    {
+      config,
+      lib,
+      ...
+    }:
+    {
+      options.custom.system.sample.enable = lib.mkEnableOption "system.sample";
 
+      config = lib.mkIf config.custom.system.sample.enable {
+      };
+    };
+
+  flake.hjemModules.sample =
+    {
+      config,
+      lib,
+      ...
+    }:
+    {
+      options.custom.home.sample.enable = lib.mkEnableOption "home.sample";
+
+      config = lib.mkIf config.custom.home.sample.enable {
+      };
+    };
 }
