3e5794c0f336e6d36d8fdf3894f0c473575aaf47
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-08-01 06:18:10 +0000

    fix: disable a few minima bits

--- machines/minima/specialisations/_vmware-guest.nix
+++ machines/minima/specialisations/_vmware-guest.nix
@@ -5,6 +5,9 @@
       specialisation."VMWare Guest".configuration = {
         virtualisation.vmware.guest.enable = true;
         custom.system.vmware-shared.enable = true;
+
+	custom.system.grub.enable = true;
+
       };
     };
 }
