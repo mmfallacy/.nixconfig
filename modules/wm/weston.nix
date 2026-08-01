911ef726afbeb4459e3425fbaa237e0b95032fff
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-08-01 02:51:12 +0000

    weston: add kitty as terminal

--- modules/wm/weston.nix
+++ modules/wm/weston.nix
@@ -21,4 +21,36 @@
         ];
       };
     };
+
+  flake.hjemModules.kitty =
+    {
+      osConfig,
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      icon =
+        pkgs.runCommand "kitty-icon-32.png"
+          {
+            nativeBuildInputs = [ pkgs.imagemagick ];
+          }
+          ''
+            magick ${pkgs.kitty}/share/icons/hicolor/256x256/apps/kitty.png -resize 32x32 $out
+          '';
+    in
+    {
+      # Use kitty as default shell
+      config = lib.mkIf osConfig.custom.system.weston.enable {
+        xdg.config.files."weston.ini" = {
+          generator = lib.generators.toINI { };
+          value = {
+            launcher = {
+              icon = icon;
+              path = "${lib.getExe pkgs.kitty}";
+            };
+          };
+        };
+      };
+    };
 }
