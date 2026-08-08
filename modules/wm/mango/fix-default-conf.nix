2e3f67f6b3e6d523d37c2a8a4897e07d56ae373a
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2026-08-08 02:29:13 +0000

    fix: mango: fix-default-conf

--- modules/wm/mango/fix-default-conf.nix
+++ modules/wm/mango/fix-default-conf.nix
@@ -7,9 +7,7 @@
       ...
     }:
     {
-      options.custom.system.mango.enable = lib.mkEnableOption "system.mango";
-
-      config = lib.mkIf config.custom.system.niri.enable {
+      config = lib.mkIf config.custom.system.mango.enable {
         # default config isn't copied from pkgs.mango for some reason
 
         environment.etc."mango/config.conf".source = "${pkgs.mangowc}/etc/mango/config.conf";
